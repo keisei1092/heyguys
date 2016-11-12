1d257a8898ec7eba731518b0b39d1b9701c8ceec
keisei_1092 <keisei_1092@yahoo.co.jp>
2016-11-12 22:50:27 +0900

    きじかなりみやすく

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -6,7 +6,7 @@ module ArticlesHelper
   def markdown(text)
     options = {
         # filter_html:     true,
-        # hard_wrap:       true,
+        hard_wrap:       true,
         # link_attributes: { rel: 'nofollow', target: "_blank" },
         # space_after_headers: true,
         # fenced_code_blocks: true
